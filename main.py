20509dc4e768b2b6dba4f388210a7d0085aa290a
Rebekka <1921861@stud.hs-mannheim.de>
2026-06-21 11:02:40 +0200

    calls models

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import torch, torchaudio, numpy as np
-from transformers import Wav2Vec2Model, Wav2Vec2FeatureExtractor
+from transformers import AutoTokenizer, AutoModel, AutoProcessor, AutoModelForZeroShotImageClassification
 
 #TODO: pretrained Models
 # Text sentence-transformers/all-MiniLM-L6-v2 384
@@ -14,4 +14,14 @@ from transformers import Wav2Vec2Model, Wav2Vec2FeatureExtractor
 
 if __name__ == "__main__":
     text_model_name = "sentence-transformers/all-MiniLM-L6-v2"
-    bild_model_name = "openai/clip-vit-base-patch32"
+    image_model_name = "openai/clip-vit-base-patch32"
+
+    #text_tokenizer = AutoTokenizer.from_pretrained(text_model_name)
+    text_model = AutoModel.from_pretrained(text_model_name)
+
+    image_processor = AutoProcessor.from_pretrained(image_model_name)
+    image_model = AutoModelForZeroShotImageClassification.from_pretrained(image_model_name)
+
+    # long_text = "This is a very long text. " * 100
+    # inputs = text_tokenizer(long_text, truncation=True, max_length=128)
+    # print(inputs)
