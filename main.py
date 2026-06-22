5dba69c97d6f0b41ea09427e58ccc02f3a8eed36
Rebekka <1921861@stud.hs-mannheim.de>
2026-06-22 15:15:46 +0200

    file loading from dir finally working....

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import torchaudio
 import numpy as np
 from transformers import AutoTokenizer, AutoModel, AutoProcessor, AutoModelForZeroShotImageClassification
 
+from extract_features import ExtractFeatures
+from helper.directory_functions import get_root
+
 #TODO: pretrained Models
 # Text sentence-transformers/all-MiniLM-L6-v2 384
 # Bild openai/clip-vit-base-patch32 512
@@ -15,17 +18,23 @@ from transformers import AutoTokenizer, AutoModel, AutoProcessor, AutoModelForZe
 # Early Fusion (Konkatenation, optional mit Projektion auf gemeinsame Dimension)
 
 if __name__ == "__main__":
-    # https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2
-    text_model_name = "sentence-transformers/all-MiniLM-L6-v2"
-    # https://huggingface.co/openai/clip-vit-base-patch32?library=transformers
-    image_model_name = "openai/clip-vit-base-patch32"
 
-    #text_tokenizer = AutoTokenizer.from_pretrained(text_model_name)
-    text_model = AutoModel.from_pretrained(text_model_name)
+    extract_features = ExtractFeatures()
+    datensatz = extract_features.load_dataset_from_dir()
 
-    image_processor = AutoProcessor.from_pretrained(image_model_name)
-    image_model = AutoModelForZeroShotImageClassification.from_pretrained(image_model_name)
+    print(datensatz['train'].features) # 'train'
 
-    # long_text = "This is a very long text. " * 100
-    # inputs = text_tokenizer(long_text, truncation=True, max_length=128)
-    # print(inputs)
+    # # https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2
+    # text_model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    # # https://huggingface.co/openai/clip-vit-base-patch32?library=transformers
+    # image_model_name = "openai/clip-vit-base-patch32"
+    #
+    # #text_tokenizer = AutoTokenizer.from_pretrained(text_model_name)
+    # text_model = AutoModel.from_pretrained(text_model_name)
+    #
+    # image_processor = AutoProcessor.from_pretrained(image_model_name)
+    # image_model = AutoModelForZeroShotImageClassification.from_pretrained(image_model_name)
+    #
+    # # long_text = "This is a very long text. " * 100
+    # # inputs = text_tokenizer(long_text, truncation=True, max_length=128)
+    # # print(inputs)
--- src/extract_features.py
+++ src/extract_features.py
@@ -1,40 +1,27 @@
+import os.path
+
 from datasets import load_dataset, load_from_disk, DatasetDict, Dataset
 
-from helper.directory_functions import is_dataset_dir_existing
+from helper.directory_functions import is_dataset_dir_existing, create_dir_name, get_root
 
 
 class ExtractFeatures:
-    DATASET_DIR = "../data/dataset/"
+    DATASET_DIR = os.path.join(get_root(), "data", "dataset")
     DATASET_NAME = ""
 
     def __init__(self, dataset_name="Leonardo6/memotion"):
         self.dataset_name = dataset_name
-        self.dataset_dir_name = _create_dir_name(self.dataset_name)
+        self.dataset_dir_name = create_dir_name(self.dataset_name)
         self._load_and_save_dataset()
 
-    def _load_and_save_dataset(self):
+    def _load_and_save_dataset(self) -> None:
         if not is_dataset_dir_existing(self.dataset_dir_name):
             cur_dataset = load_dataset(self.dataset_name)
-            cur_dataset.save_to_disk(self.DATASET_DIR + self.dataset_dir_name)
+            cur_dataset.save_to_disk(os.path.join(self.DATASET_DIR, self.dataset_dir_name))
 
     def load_dataset_from_dir(self, dataset_name="Leonardo6/memotion") -> Dataset | DatasetDict | None:
-        if is_dataset_dir_existing(_create_dir_name(dataset_name)):
-            return load_from_disk(self.DATASET_DIR + self.dataset_dir_name)
+        if is_dataset_dir_existing(create_dir_name(dataset_name)):
+            return load_from_disk(os.path.join(self.DATASET_DIR, self.dataset_dir_name))
         else:
             return None
 
-
-def _create_dir_name(dataset_name):
-    name_list = dataset_name.split("/")
-    dir_name = ""
-    for items in name_list:
-        dir_name += (items + "_")
-
-    return dir_name
-
-
-if __name__ == "__main__":
-
-    extractor = ExtractFeatures()
-    ds = extractor.load_dataset_from_dir()
-    print("meme")
