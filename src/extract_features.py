4e0d2093c893e18f29e6cab65108cd4d1e3e69ca
Rebekka <1921861@stud.hs-mannheim.de>
2026-06-23 13:46:35 +0200

    loading dataset online not automatically called on creating object -> separate method with more checking if data already exists, same goes for local loading of data -> added more checking

--- src/extract_features.py
+++ src/extract_features.py
@@ -14,16 +14,19 @@ class ExtractFeatures:
         self.dataset_dir_name = create_dir_name(self.dataset_name)
         self._is_loaded_locally = is_dataset_dir_existing(self.dataset_dir_name)
 
-    def load_and_save_dataset(self, dataset_name) -> None:
+    def load_and_save_dataset(self, dataset_name) -> bool | None:
         if not self._is_loaded_locally:
             if dataset_name == self.dataset_name:
                 cur_dataset = load_dataset(self.dataset_name)
                 cur_dataset.save_to_disk(os.path.join(self.DATASET_DIR, self.dataset_dir_name))
                 self._is_loaded_locally = is_dataset_dir_existing(self.dataset_dir_name)
+                return True
             else:
                 print(f"{dataset_name} is not this dataset")
+                return None
         else:
             print("Dataset is already loaded locally")
+            return None
 
     def load_dataset_from_dir(self, dataset_name="Leonardo6/memotion") -> Dataset | DatasetDict | None:
         if not self._is_loaded_locally:
