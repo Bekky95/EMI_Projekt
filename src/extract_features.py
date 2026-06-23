8ee62e185031cd87b08cbe16135424d5acd67bca
Rebekka <1921861@stud.hs-mannheim.de>
2026-06-23 13:43:35 +0200

    loading dataset online not automatically called on creating object -> separate method with more checking if data already exists, same goes for local loading of data -> added more checking

--- src/extract_features.py
+++ src/extract_features.py
@@ -12,16 +12,29 @@ class ExtractFeatures:
     def __init__(self, dataset_name="Leonardo6/memotion"):
         self.dataset_name = dataset_name
         self.dataset_dir_name = create_dir_name(self.dataset_name)
-        self._load_and_save_dataset()
+        self._is_loaded_locally = is_dataset_dir_existing(self.dataset_dir_name)
 
-    def _load_and_save_dataset(self) -> None:
-        if not is_dataset_dir_existing(self.dataset_dir_name):
-            cur_dataset = load_dataset(self.dataset_name)
-            cur_dataset.save_to_disk(os.path.join(self.DATASET_DIR, self.dataset_dir_name))
+    def load_and_save_dataset(self, dataset_name) -> None:
+        if not self._is_loaded_locally:
+            if dataset_name == self.dataset_name:
+                cur_dataset = load_dataset(self.dataset_name)
+                cur_dataset.save_to_disk(os.path.join(self.DATASET_DIR, self.dataset_dir_name))
+                self._is_loaded_locally = is_dataset_dir_existing(self.dataset_dir_name)
+            else:
+                print(f"{dataset_name} is not this dataset")
+        else:
+            print("Dataset is already loaded locally")
 
     def load_dataset_from_dir(self, dataset_name="Leonardo6/memotion") -> Dataset | DatasetDict | None:
-        if is_dataset_dir_existing(create_dir_name(dataset_name)):
-            return load_from_disk(os.path.join(self.DATASET_DIR, self.dataset_dir_name))
+        if not self._is_loaded_locally:
+            if dataset_name == self.dataset_name:
+                return load_from_disk(os.path.join(self.DATASET_DIR, self.dataset_dir_name))
+            else:
+                print(f"{dataset_name} is not this dataset")
+                return None
         else:
+            print("Dataset is already loaded locally")
             return None
 
+    def is_dataset_loaded_locally(self) -> bool:
+        return self._is_loaded_locally
